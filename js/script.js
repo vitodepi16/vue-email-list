d9c3e62991e34c1e8b0e16cde9967d302d40eebc
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-04-11 14:39:19 +0200

    end exercise with axios librery

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@ const { createApp }= Vue;
 createApp({
     data(){
         return{
-            
+            basePath: 'https://flynn.boolean.careers/exercises/api',
+            emails: []
+        }
+    },
+    methods: {
+        getEmail(){
+            this.emails = [];
+            for (let i = 0; i < 10; i++){
+                axios.get(`${this.basePath}/random/mail`).then((result)=> {
+                    this.emails.push(result.data.response);
+                }) 
+            }
         }
     }
 }).mount('#app')
